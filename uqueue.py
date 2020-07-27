b568e55697412449a3eb982fb6434d321ed3f9a6
Dizzy-codes <68827682+Dizzy-codes@users.noreply.github.com>
2020-07-26 21:21:11 -0400

    Update uqueue.py

--- uqueue.py
+++ uqueue.py
@@ -73,16 +73,19 @@ async def MyPlace(ctx):
     author_ctx = ctx.message.author.mention
     #Grab the index of the user, add 1 to the index because python starts the index at 0
     placement = queue.index(author_ctx) + 1
-    if len(queue) == 0:
-        await ctx.send("`No one is currently in the queue!`")
-    else:
-        embed = discord.Embed(
-            title = f"**Specific Position in Queue:**",
-            description = f"{author_ctx}, your placement in the queue is: **{placement}**",
-            colour = discord.Colour.purple(),
-        )
-        embed.timestamp = datetime.datetime.utcnow()
-        await ctx.send(embed=embed)
+    try:
+        if len(queue) == 0:
+            await ctx.send("`No one is currently in the queue!`")
+        else:
+            embed = discord.Embed(
+                title = f"**Specific Position in Queue:**",
+                description = f"{author_ctx}, your placement in the queue is: **{placement}**",
+                colour = discord.Colour.purple(),
+            )
+            embed.timestamp = datetime.datetime.utcnow()
+            await ctx.send(embed=embed)
+    except ValueError:
+        await ctx.send(f"{author_ctx}, you're not in the queue!")
 
 @client.command(pass_context=True)
 async def QueueSize(ctx):
